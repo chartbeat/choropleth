31a02dbcef9b60b663fdc47df441a00a2e9f027a
Allan Beaufour <allan@chartbeat.com>
2012-09-14 10:56:33 -0400

    Change into chartbeat version

--- main.js
+++ main.js
@@ -1,119 +1,177 @@
-var map = L.map('map').setView([37.8, -96], 4);
+// heavily "inspired" by: http://leaflet.cloudmade.com/examples/choropleth.html
 
-var cloudmade = L.tileLayer('http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png', {
-			      attribution: 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
-			      key: 'BC9A493B41014CAABB98F0471D759707',
-			      styleId: 22677
-		            }).addTo(map);
+var Map = {
+  // Update backend data every X ms
+  interval: 3000,
+  
+  // get color depending on visitors value
+  getColor: function(d) {
+    return d > 1000 ? '#800026' :
+      d > 500  ? '#BD0026' :
+      d > 200  ? '#E31A1C' :
+      d > 100  ? '#FC4E2A' :
+      d > 50   ? '#FD8D3C' :
+      d > 20   ? '#FEB24C' :
+      d > 10   ? '#FED976' :
+      '#FFEDA0';
+  },
 
+  style: function(feature) {
+    return {
+      weight: 2,
+      opacity: 1,
+      color: 'white',
+      dashArray: '3',
+      fillOpacity: 0.7,
+      fillColor: this.getColor(feature.properties.visitors)
+    };
+  },
 
-// control that shows state info on hover
-var info = L.control();
+  highlightFeature: function(e) {
+    var layer = e.target;
 
-info.onAdd = function (map) {
-  this._div = L.DomUtil.create('div', 'info');
-  this.update();
-  return this._div;
-};
+    layer.setStyle({
+                     weight: 5,
+                     color: '#666',
+                     dashArray: '',
+                     fillOpacity: 0.7
+                   });
 
-info.update = function (props) {
-  this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
-				                             '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
-				                             : 'Hover over a state');
-};
-
-info.addTo(map);
-
-
-// get color depending on population density value
-function getColor(d) {
-  return d > 1000 ? '#800026' :
-    d > 500  ? '#BD0026' :
-    d > 200  ? '#E31A1C' :
-    d > 100  ? '#FC4E2A' :
-    d > 50   ? '#FD8D3C' :
-    d > 20   ? '#FEB24C' :
-    d > 10   ? '#FED976' :
-    '#FFEDA0';
-}
-
-function style(feature) {
-  return {
-    weight: 2,
-    opacity: 1,
-    color: 'white',
-    dashArray: '3',
-    fillOpacity: 0.7,
-    fillColor: getColor(feature.properties.density)
-  };
-}
-
-function highlightFeature(e) {
-  var layer = e.target;
-
-  layer.setStyle({
-		   weight: 5,
-		   color: '#666',
-		   dashArray: '',
-		   fillOpacity: 0.7
-		 });
-
-  if (!L.Browser.ie && !L.Browser.opera) {
-    layer.bringToFront();
-  }
-
-  info.update(layer.feature.properties);
-}
-
-var geojson;
-
-function resetHighlight(e) {
-  geojson.resetStyle(e.target);
-  info.update();
-}
-
-function zoomToFeature(e) {
-  map.fitBounds(e.target.getBounds());
-}
-
-function onEachFeature(feature, layer) {
-  layer.on({
-	     mouseover: highlightFeature,
-	     mouseout: resetHighlight,
-	     click: zoomToFeature
-	   });
-}
-
-function main() {
-  geojson = L.geoJson(statesData, {
-		        style: style,
-		        onEachFeature: onEachFeature
-		      }).addTo(map);
-
-  map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
-
-
-  var legend = L.control({position: 'bottomright'});
-
-  legend.onAdd = function (map) {
-
-    var div = L.DomUtil.create('div', 'info legend'),
-    grades = [0, 10, 20, 50, 100, 200, 500, 1000],
-    labels = [],
-    from, to;
-
-    for (var i = 0; i < grades.length; i++) {
-      from = grades[i];
-      to = grades[i + 1];
-
-      labels.push(
-        '<i style="background:' + getColor(from + 1) + '"></i> ' +
-	  from + (to ? '&ndash;' + to : '+'));
+    if (!L.Browser.ie && !L.Browser.opera) {
+      layer.bringToFront();
     }
 
-    div.innerHTML = labels.join('<br>');
-    return div;
-  };
+    this.info.update(layer.feature.properties);
+  },
 
-  legend.addTo(map);
+  resetHighlight: function(e) {
+    this.geojson.resetStyle(e.target);
+    this.info.update();
+  },
+
+  zoomToFeature: function(e) {
+    this.map.fitBounds(e.target.getBounds());
+  },
+
+  onEachFeature: function(feature, layer) {
+    layer.on({
+               mouseover: $.proxy(this.highlightFeature, this),
+               mouseout: $.proxy(this.resetHighlight, this),
+               click: $.proxy(this.zoomToFeature, this)
+             });
+  },
+
+  setupMap: function() {
+    this.map = L.map('map').setView([37.8, -96], 4);
+
+
+    this.info = L.control();
+    this.info.onAdd = function (map) {
+      this._div = L.DomUtil.create('div', 'info');
+      this.update();
+      return this._div;
+    };
+    this.info.update = function (props) {
+      this._div.innerHTML = '<h4>Number of Visitors</h4>' +  (props ?
+                                                              '<b>' + props.name + '</b><br />' + props.visitors + ' visitors'
+                                                              : 'Hover over a state');
+    };
+    this.info.addTo(this.map);
+
+    var tileTemplate = 'http://{s}.tiles.mapbox.com/v3/chartbeat.chartbeat/{z}/{x}/{y}.png';
+
+    // Tile information
+    var tiles = new L.TileLayer(tileTemplate, {
+                                  'attribution': 'Map data © OpenStreetMap, Imagery © MapBox',
+                                  'minZoom': 2,
+                                  'maxZoom': 8,
+                                  'subdomains': 'abcd',
+                                  'unloadInvisibleTiles': true,
+                                  'reuseTiles': true
+                                }).addTo(this.map);
+
+    this.geojson = L.geoJson(statesData, {
+                               style: $.proxy(this.style, this),
+                               onEachFeature: $.proxy(this.onEachFeature, this)
+                             }).addTo(this.map);
+
+    var legend = L.control({position: 'bottomright'});
+
+    var getColor = $.proxy(this.getColor, this);
+    legend.onAdd = function (map) {
+      var div = L.DomUtil.create('div', 'info legend'),
+      grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+      labels = [],
+      from, to;
+
+      for (var i = 0; i < grades.length; i++) {
+        from = grades[i];
+        to = grades[i + 1];
+
+        labels.push(
+          '<i style="background:' + getColor(from + 1) + '"></i> ' +
+            from + (to ? '&ndash;' + to : '+'));
+      }
+
+      div.innerHTML = labels.join('<br>');
+      return div;
+    };
+
+    legend.addTo(this.map);
+  },
+
+  refreshMap: function() {
+    this.geojson.clearLayers();
+    this.geojson.addData(statesData, {
+                           style: $.proxy(this.style, this),
+                           onEachFeature: $.proxy(this.onEachFeature, this)
+                         });
+  },
+
+  parseQueryString: function() {
+    var ret = {};
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+      ret[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+    }
+    return ret;
+  },
+
+  setupUrl: function() {
+    var queryVars = this.parseQueryString();
+    var domain = queryVars["host"] || "gizmodo.com";
+    var apikey = queryVars["apikey"] || "317a25eccba186e0f6b558f45214c0e7";
+    var limit = 5000;
+    this.api_endpoint = "http://api.chartbeat.com/geo/?host=" + domain + "&apikey=" + apikey + "&limit=" + limit + "&country=US";
+  },
+
+  updateData: function() {
+    function onSuccess(response) {
+        for (var i = 0; i < statesData.features.length; ++i) {
+          var state = statesData.features[i].properties["abbr"];
+          var val = response["regions"][state];
+          statesData.features[i].properties["visitors"] = val || 0;
+        }
+        this.refreshMap();
+      setTimeout($.proxy(this.updateData, this), this.interval);
+    }
+
+    function onError() {
+      setTimeout($.proxy(this.updateData, this), this.interval);
+    }
+
+    $.ajax({
+             url: this.api_endpoint,
+             success: $.proxy(onSuccess, this),
+             error: $.proxy(onError, this)
+           });
+  },
+
+  start: function() {
+    this.setupUrl();
+    this.setupMap();
+    this.updateData();
+  }
 };
